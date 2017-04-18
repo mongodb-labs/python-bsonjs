612ea8f650bac65230303fa05ccfd08ae0759692
Shane Harvey <shane.harvey@mongodb.com>
2017-04-18 16:25:06 -0700

    Add travis manylinux wheel testing and fix undefined symbol: clock_gettime

---
+++ build-wheels.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "$0 requires one argument: <BSONJS_SOURCE_DIRECTORY>"
+    echo "For example: $0 /user/home/git/python-bsonjs"
+    exit 1
+fi
+
+set -e -x
+
+BSONJS_SOURCE_DIRECTORY="$1"
+cd "$BSONJS_SOURCE_DIRECTORY"
+
+ls -la
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/python" setup.py bdist_wheel
+    # https://github.com/pypa/manylinux/issues/49
+    rm -rf build
+done
+ 
+# Audit wheels and write multilinux1 tag
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install python-bsonjs --no-index -f dist
+    # The tests require PyMongo.
+    "${PYBIN}/pip" install 'pymongo>=3.4' unittest2
+    for TEST_FILE in "${BSONJS_SOURCE_DIRECTORY}"/test/test_*.py; do
+        "${PYBIN}/python" "$TEST_FILE" -v
+    done
+done
+
+ls -lah dist
