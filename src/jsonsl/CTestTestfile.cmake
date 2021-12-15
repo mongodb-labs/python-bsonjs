fb3516ecbbc2ec736050f401c4d147043fcd3342
julius <juliusgeo@gmail.com>
2021-12-14 17:26:27 -0800

    remove more cmake files

--- src/jsonsl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/julius/Work/mongo-c-driver/src/libbson/src/jsonsl
-# Build directory: /Users/julius/Work/mongo-c-driver/cmake-build/src/libbson/src/jsonsl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
