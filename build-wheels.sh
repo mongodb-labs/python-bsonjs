e5a70a396344085e4a6c71ef161319d152676d26
julius <juliusgeo@gmail.com>
2022-01-03 19:22:23 -0800

    some more fixes to scripting to hopefully fix issues with wheelbuilding

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,2 +1,38 @@
 #!/bin/bash -ex
 
+if [ "$#" -ne 1 ]; then
+    echo "$0 requires one argument: <BSONJS_SOURCE_DIRECTORY>"
+    echo "For example: $0 /user/home/git/python-bsonjs"
+    exit 1
+fi
+
+BSONJS_SOURCE_DIRECTORY="$1"
+cd "$BSONJS_SOURCE_DIRECTORY"
+
+ls -la
+$PYTHON_BINARY --version
+$PYTHON_BINARY -m pip install wheel
+# Build limited abi3 wheel.
+$PYTHON_BINARY setup.py bdist_wheel
+# https://github.com/pypa/manylinux/issues/49
+rm -rf build
+
+# Audit wheels and write multilinux1 tag
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist
+done
+
+# Install packages and test.
+for PYBIN in /opt/python/*/bin; do
+    if [[ ! "${PYBIN}" =~ (36|37|38|39|310) ]]; then
+        continue
+    fi
+    "${PYBIN}/pip" install python-bsonjs --no-index -f dist
+    # The tests require PyMongo.
+    "${PYBIN}/pip" install 'pymongo>=3.4'
+    for TEST_FILE in "${BSONJS_SOURCE_DIRECTORY}"/test/test_*.py; do
+        "${PYBIN}/python" "$TEST_FILE" -v
+    done
+done
+
+ls -lah dist
